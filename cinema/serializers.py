97c3060af890f40f03676d59e74a4866ed6f16b6
Dolgov Dmytro <dimakherson74@gmail.com>
2025-03-30 22:11:40 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -119,7 +119,7 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ("id", "tickets", "created_at")
+        fields = ("id", "tickets", "created_at", "user")
 
     def create(self, validated_data):
         with transaction.atomic():
@@ -135,4 +135,4 @@ class OrderListSerializer(OrderSerializer):
 
     class Meta:
         model = Order
-        fields = ("id", "tickets", "created_at")
+        fields = ("id", "tickets", "created_at", "user")
